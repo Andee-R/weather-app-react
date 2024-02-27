d80548948d63ff4f8a548dbb72070b81e70c91e1
Andee-R <aqueen.reid@gmail.com>
2024-02-27 08:42:14 -0700

    added github and netlify links

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 import "./App.css";
+import SearchForm from "./SearchForm";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Weather App</h1>
-        <footer>
-          <a
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer">
-            React
-          </a>
-        </footer>
+        <SearchForm />
+        <div className="Portfolio-links">
+          <p>
+            Open sorced on{" "}
+            <a
+              href="https://github.com/Andee-R/weather-app-react"
+              target="_blank"
+              rel="noopener noreferrer">
+              Github
+            </a>
+            , hosted on{" "}
+            <a
+              href="https://tranquil-scone-a86042.netlify.app/"
+              target="_blank"
+              rel="noopener noreferrer">
+              Netlify
+            </a>
+          </p>
+        </div>
       </header>
     </div>
   );
---
+++ src/SearchForm.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function SearchForm() {
+  return (
+    <form>
+      <input type="Search" placeholder="Search City..." />
+      <button type="Submit">Submit</button>
+    </form>
+  );
+}
